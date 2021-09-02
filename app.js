200466badd658a7f3a8af328fa8bd883e7da29e6
Iasir-Arafat-riaz <76946854+Iasir-Arafat-riaz@users.noreply.github.com>
2021-09-02 07:53:24 +0600

    over all commit

--- app.js
+++ app.js
@@ -1,38 +1,65 @@
 const searchField = document.getElementById("search-field");
+const bookParentDiv = document.getElementById("books-parent");
+const bookFound = document.getElementById("bookFound")
+bookFound.innerText=""
 
 
 const searchButton = async () => {
+  
   const fieldValue = searchField.value;
-  const url = `http://openlibrary.org/search.json?q=${fieldValue}`;
+  const url = `https://openlibrary.org/search.json?q=${fieldValue}`;
   const respo = await fetch(url);
   const data = await respo.json();
+  
   allBooks(data);
-  searchField.value=""
+  
+  
 };
+//declare arrow function for bonus requirement
 const allBooks = (books) => {
-  console.log(books);
-  const bookParentDiv = document.getElementById("books-parent");
-  bookParentDiv.textContent=""
-  const bookFound = document.getElementById("bookFound")
-  bookFound.innerText= `totalbook found ${books.numFound}`
-  const booksArray = books.docs;
-  booksArray.forEach((book) => {
-    console.log(book)
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("col-lg-3");
+  //console.log(books);
+  //apply triple equal for bonus requirement
+  if(searchField.value===""){
+    bookParentDiv.textContent=""
+    bookFound.style.color="orange"
+    bookFound.innerText="OOOPS..you did't write anything..please write book name"
+    console.log("empty")
+  }
+  //apply triple equal for bonus requirement
+  else if(books.numFound===0){
+    searchField.value=""
+    bookParentDiv.textContent=""
+    bookFound.style.color="red"
+    bookFound.innerText="No result found"
     
-    bookCard.innerHTML = `
-                      <div class="card">
-                         <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h4 class="card-title">Book Name: ${book.title}</h4>
-                          <h6>Writer: ${book.author_name} </h6>
-                          <p>Publisher: ${book.publisher[0]}</p>
-                          <p>First published Year: ${book.first_publish_year} </p>
-                          
+   
+  }
+  else{
+    searchField.value=""
+    bookParentDiv.textContent=""
+    bookFound.style.color="green"
+    
+    bookFound.innerText= `Total Book found: ${books.numFound}`
+    const booksArray = books.docs;
+    //apply forEach for bonus requirement
+    booksArray.forEach((book) => {
+      console.log(book)
+      const bookCard = document.createElement("div");
+      bookCard.classList.add("col-lg-3");
+      
+      bookCard.innerHTML = `
+                        <div class="card">
+                           <img class="image" src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
+                          <div class="card-body">
+                             <h6 class="card-title">Book Name: <span>${book.title}</span></h6>
+                             <p>Writer: <span>${book.author_name[0]}</span> <p>
+                             <p>Publisher: <span>${book.publisher[0]} </span></p>
+                             <p>First published Year: <span>${book.first_publish_year}<span> </p>
+                         </div>
                         </div>
-                      </div>
-        `;
-        bookParentDiv.appendChild(bookCard)
-  });
+          `;
+          bookParentDiv.appendChild(bookCard)
+    });
+  }
+ 
 };
